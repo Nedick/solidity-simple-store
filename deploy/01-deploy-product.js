f3e76b8b5052c8d4a0864f239ab7802f012f543f
Nedelin Slavov <nedelin.slavov@gmail.com>
2022-08-30 19:54:22 +0300

    Added deployment scripts for Product and SimpleStore contracts

---
+++ deploy/01-deploy-product.js
@@ -0,0 +1,25 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    log("------------------------------")
+    const args = ["some://random.uri"]
+    const product = await deploy("Product", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying contract...")
+        await verify(product.address, args)
+    }
+    log("------------------------------")
+}
+
+module.exports.tags = ["all", "product", "main"]
