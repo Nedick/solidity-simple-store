c27f53429fc3e2a974faabaf0eb1537cb2787e46
Nedelin Slavov <nedelin.slavov@gmail.com>
2022-08-31 19:17:45 +0300

    Updated hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ require("solidity-coverage")
 require("hardhat-deploy")
 
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || ""
 const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
 const REPORT_GAS = process.env.REPORT_GAS || false
@@ -20,7 +20,7 @@ module.exports = {
         },
         rinkeby: {
             url: RINKEBY_RPC_URL,
-            accounts: [PRIVATE_KEY],
+            accounts: [`${PRIVATE_KEY}`],
             chainId: 4,
             blockConfirmations: 6,
         },
