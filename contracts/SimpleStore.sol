a161a578231c8e193f4c3ee9427cdbbf7d78a025
Nedelin Slavov <nedelin.slavov@gmail.com>
2022-09-01 18:24:21 +0300

    Removing the task description from contract

--- contracts/SimpleStore.sol
+++ contracts/SimpleStore.sol
@@ -6,20 +6,6 @@ import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./Product.sol";
 
-/** 
-Using Remix develop a contract for a TechnoLime Store.
-
-- The administrator (owner) of the store should be able to add new products and the quantity of them. -Done!
-- The administrator should not be able to add the same product twice, just quantity.                  -Done!
-- Buyers (clients) should be able to see the available products and buy them by their id.             -Done!
-- Buyers should be able to return products if they are not satisfied
-(within a certain period in blocktime: 100 blocks).                                                   
-- A client cannot buy the same product more than one time.                                            -Done!
-- The clients should not be able to buy a product more times than the quantity in the store           
-unless a product is returned or added by the administrator (owner)                                    -Done!
-- Everyone should be able to see the addresses of all clients that have ever bought a given product.  -Done!
-*/
-
 error SimpleStore__AlreadyBoughtThatItem();
 error SimpleStore__PriceTooLow();
 error SimpleStore__NotAvailableForReturn();
